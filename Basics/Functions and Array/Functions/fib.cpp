4e6d805fcd309b0102e07208ea5e58298680e30b
Sachin Gupta <sgupta15120@gmail.com>
2022-10-31 07:58:44 +0530

    changes

--- Basics/Functions and Array/Functions/fib.cpp
+++ Basics/Functions and Array/Functions/fib.cpp
@@ -1,18 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void fibonaci(int n){
-    int i = 0,j = 1;
-    for(i)
+void fibonaci(int n)
+{
+    int i = -1, j = 1;
+    //cout << i << " " << j << " ";
+    int sum = 0;
+    for (int a = 0; a < n; a++)
+    {
+        sum = i + j;
+        cout << sum << " ";
+        i = j;
+        j = sum;
+    }
 }
 
 int main()
+// void main()--->error: '::main' must return 'int'
+//  void main()
 {
     int number;
-    cout << "Enter the number "<<endl;
+    cout << "Enter the number " << endl;
     cin >> number;
     cout << "Fibonaci series : ";
     fibonaci(number);
 
     return 0;
-}
+} // 0  1  1 2 3 5
