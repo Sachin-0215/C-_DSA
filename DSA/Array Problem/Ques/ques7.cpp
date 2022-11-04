fe507cd2e11b5ae9edd5e43668db6b159c2c5cc1
Sachin Gupta <sgupta15120@gmail.com>
2022-11-04 11:00:48 +0530

    modify

--- DSA/Array Problem/Ques/ques7.cpp
+++ DSA/Array Problem/Ques/ques7.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <unordered_set>
 using namespace std;
 
 vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
@@ -63,43 +64,65 @@ vector<int> intersection_of_array(vector<int> &nums1, vector<int> &nums2)
 // union of two array
 vector<int> union_of_array(vector<int> &v1, vector<int> &v2)
 {
-    vector<int> s;
+    // vector<int> s;
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
-    int ptr1 = 0, ptr2 = 0;
-    int size1 = v1.size(), size2 = v2.size();
+    // int ptr1 = 0, ptr2 = 0;
+    // int size1 = v1.size(), size2 = v2.size();
 
-    while (ptr1 < size1 && ptr2 < size2)
+    // while (ptr1 < size1 && ptr2 < size2)
+    // {
+    //     if (v1[ptr1] > v2[ptr2])
+    //     {
+    //         s.push_back(v2[ptr2]);
+    //         ptr2++;
+    //     }
+    //     else if (v1[ptr1] < v2[ptr2])
+    //     {
+    //         s.push_back(v1[ptr1]);
+    //         ptr1++;
+    //     }
+    //     else if (v1[ptr1++] == v2[ptr2++])
+    //     {
+    //         s.push_back(v1[ptr1]);
+    //     }
+    // }
+    // printing the union aray
+    // for (int i = 0; i < s.size();i++){
+    //     cout << s[i] << " ";
+    // }
+    // cout << endl;
+    // return s;
+    unordered_set<int> s;
+    for (int i = 0; i < v1.size(); i++)
     {
-        if (v1[ptr1] > v2[ptr2])
-        {
-            s.push_back(ptr2);
-            ptr2++;
-        }
-        else if (v1[ptr1] < v2[ptr2])
-        {
-            s.push_back(ptr1);
-            ptr1++;
-        }
-        else if (v1[ptr1++] == v2[ptr2++])
-        {
-            s.push_back(ptr1);
+        s.insert(v1[i]);
+    }
+    for (int i = 0; i < v2.size(); i++)
+    {
+        if(!s.count(v2[i])){
+            s.insert(v2[i]);
         }
     }
-    
-    return s;
+    vector<int> ans;
+    for(auto i:s){
+        ans.push_back(i);
+    }
+    for (int i = 0; i < ans.size();i++){
+        cout << ans[i]<<" ";
+    }
+        return ans;
 }
 
 // 3 4 5 8 9
-// 2 4 5 7 8 9
-// 2 3 4 5 7 8
+// 2 4 5 7 8 9  
+// 2 3 4 5 7 8 9
 int main()
 {
     vector<int> nums1{4, 3, 9, 5, 8};
     vector<int> nums2{5, 7, 9, 4, 8, 2};
     // intersection(nums1, nums2);
     union_of_array(nums1, nums2);
-    
 
     return 0;
 }
